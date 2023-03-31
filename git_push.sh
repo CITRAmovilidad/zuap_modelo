fbed599e7bebf17c00f33eef7a158c84a5f57c68
CITRAmovilidad <smm.citra@gmail.com>
2023-03-31 16:56:44 -0500

    prueba4

--- git_push.sh
+++ git_push.sh
@@ -5,3 +5,7 @@ fecha=$(date '+%Y-%m-%d')
 git add distribucion_multinomial_grafico.pkl
 git commit -m "Actualización diaria del archivo distribucion_multinomial_grafico.pkl - $fecha"
 git push https://github.com/CITRAmovilidad/PrediccionZUAP.git master
+
+
+
+
